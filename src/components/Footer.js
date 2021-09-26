e987e6b4dc0113e5b3f5bddd1157345ca7e3733f
Shivani-Jadon <shivanijadon98@gmail.com>
2021-09-26 18:56:34 +0530

    header and footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,36 @@
-import React from 'react'
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import HomeIcon from '@material-ui/icons/HomeOutlined';
+import ChatIcon from '@material-ui/icons/QuestionAnswerOutlined';
+import AskIcon from '../assets/logo/ask.png';
+import ReportIcon from '../assets/logo/reports.png'
 
-function Footer() {
-    return (
-        <div>
-            Footer
-        </div>
-    )
+
+const useStyles = makeStyles({
+  root: {
+    width: 500,
+  },
+});
+
+export default function  Footer() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  return (
+    <BottomNavigation
+      value={value}
+      onChange={(event, newValue) => {
+        setValue(newValue);
+      }}
+      showLabels
+      className={classes.root}
+    >
+      <BottomNavigationAction label="Home" icon={<HomeIcon />} />
+      <BottomNavigationAction label="Talk to Astroler" icon={<ChatIcon />} />
+      <BottomNavigationAction label="Ask as Question" icon={<img src={AskIcon} width="24px"></img>} disabled={true} />
+      <BottomNavigationAction label="Report" icon={<img src={ReportIcon} width="24px"></img>} disabled={true} />
+    </BottomNavigation>
+  );
 }
-
-export default Footer;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import * as React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Box from '@material-ui/core/Box';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '../assets/logo/hamburger.png';
+import ProfileIcon from '../assets/logo/profile.png';
+import LogoIcon from '../assets/logo/logo.png';
 
-function Header() {
-    return (
-        <div>
-            Header
-        </div>
-    )
+
+export default function ButtonAppBar() {
+  return (
+    <Box sx={{ flexGrow: 1 }} height="50px">
+      <AppBar position="fixed">
+        <Toolbar style={{justifyContent:"space-between"}}>
+          <IconButton>
+            <img src={MenuIcon}></img>
+          </IconButton>
+          <IconButton>
+              <img src={LogoIcon} height='35px'></img>
+          </IconButton>
+          <IconButton>
+              <img src={ProfileIcon} height='28px'></img>
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 }
 
-export default Header;
